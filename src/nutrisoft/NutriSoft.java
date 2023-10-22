aa1f771fb780642dde13e742a6361d60caae0b84
Wendell Marcos <wendell.marcos.code@gmail.com>
2023-10-22 02:56:48 +0100

    Projeto Senac IMC

---
+++ src/nutrisoft/NutriSoft.java
@@ -0,0 +1,20 @@
+
+package nutrisoft;
+
+import nutrisoft.telas.TelaInicial;
+
+/**
+ *
+ * @author Wendell Marcos
+ */
+public class NutriSoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         TelaInicial telaInicial = new TelaInicial();
+        telaInicial.setVisible(true);
+    }
+    
+}
